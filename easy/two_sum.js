be32c3cb4e1256a8088a0f4128c4230cb8f2a33d
rikaituannguyen <linhtuan.nguyen@rikai.technology>
2022-08-25 22:04:39 +0700

    two sum: 63 ms 43 MB

---
+++ easy/two_sum.js
@@ -0,0 +1,22 @@
+const nums = [2, 7, 11, 15]
+const target = 17
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const hash = {}
+  for (let i = 0; i < nums.length; i++) {
+    if (hash[target - nums[i]] !== undefined &&) {
+      return [hash[target - nums[i]], i]
+    }
+
+    hash[nums[i]] = i
+  }
+
+  return []
+}
+
+console.log(twoSum(nums, target))
